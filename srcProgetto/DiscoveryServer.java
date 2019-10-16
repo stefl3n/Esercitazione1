eba2cf0e9d8f824da247d1f1dcf852ede306b3ae
leone <leoneruggiero98@gmail.com>
2019-10-16 19:39:23 +0200

    gestione eccezioni

--- srcProgetto/DiscoveryServer.java
+++ srcProgetto/DiscoveryServer.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class DiscoveryServer {
 	
 	
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[])  {
 		
 		Hashtable<String, Integer> table=new Hashtable<String, Integer>(args.length-1);
 		InputStreamReader in= new InputStreamReader(System.in);
@@ -19,21 +19,52 @@ public class DiscoveryServer {
 		int portNum;
 		int hostPort;
 		
+		if((args.length%2)!=0)//controllo numero argomenti
+		{
+			System.out.println("Numero argomenti errato");
+			System.exit(-1);
+		}
+		
+		try 
+		{
 		hostPort=Integer.parseInt(args[0]);
+		if(hostPort<1024)//controllo numero porta
+			{
+				System.out.println("Inserire un numero di porta valido per il server");
+				System.exit(-1);
+			}
+			
+		
+		
 		for(int j=1; j<args.length-1; j+=2)
 		{
 			fileName=args[j];
+			
 			portNum=Integer.parseInt(args[j+1]);
-			if(table.containsValue(portNum))
-			{
-				System.out.println("Porta gia inserita:"+portNum);
-				System.exit(-1);
+				if(portNum==hostPort||portNum<1024) //controllo porte del Thread
+				{
+					System.out.println("inserire numeri di porta validi"); 
+					System.exit(-1);
+				}
+				else if(table.containsValue(portNum))//controllo unicita  porte del Thread
+					{
+						System.out.println("Porta gia inserita:"+portNum);
+						System.exit(-1);
+						
+					}
+				table.put(fileName, portNum);
+			
+			
 				
-			}
+
+		}
+		
+		
+	
 			
 
-			table.put(fileName, portNum);
-		}
+		
+		
 		for( String key : table.keySet())
 		{
 			SwapRowServer server=new SwapRowServer(key, table.get(key));
@@ -43,45 +74,65 @@ public class DiscoveryServer {
 		
 		
 		System.out.println(table.toString());
-		DatagramSocket socket = new DatagramSocket(hostPort);
-		byte[] buf = new byte[256];
-		byte[] data=new byte[256];
-		DatagramPacket packet = new DatagramPacket(buf,buf.length);
 		
-		
-		while(true) {
+			DatagramSocket socket = new DatagramSocket(hostPort);
+			byte[] buf = new byte[256];
+			byte[] data=new byte[256];
+			DatagramPacket packet = new DatagramPacket(buf,buf.length);
 			
-		packet.setData(buf);
-		socket.receive(packet);
-		ByteArrayInputStream biStream = new ByteArrayInputStream(packet.getData(),0,packet.getLength());
-		DataInputStream diStream = new DataInputStream(biStream); 
-		richiesta = diStream.readUTF();
-		StringTokenizer st = new StringTokenizer(richiesta);
-		nomeFile = st.nextToken();
-		
-		
-		
-		ByteArrayOutputStream boStream = new ByteArrayOutputStream();
-		DataOutputStream doStream = new DataOutputStream(boStream); 
-		if(table.get(nomeFile)==null)
+			while(true) {
+				
+			packet.setData(buf);
+			socket.receive(packet);
+			ByteArrayInputStream biStream = new ByteArrayInputStream(packet.getData(),0,packet.getLength());
+			DataInputStream diStream = new DataInputStream(biStream); 
+			richiesta = diStream.readUTF();
+			StringTokenizer st = new StringTokenizer(richiesta);
+			nomeFile = st.nextToken();
+			
+			
+			
+			ByteArrayOutputStream boStream = new ByteArrayOutputStream();
+			DataOutputStream doStream = new DataOutputStream(boStream); 
+			if(table.get(nomeFile)==null)//invio messaggio di errore al client
+			{
+				System.out.println("file non esistente");
+				doStream.writeUTF("-1"); 
+			}
+			
+			else
+			{
+				doStream.writeUTF(table.get(nomeFile).toString());
+				System.out.println(table.get(nomeFile).toString());
+			}
+			
+			data = boStream.toByteArray();
+			
+			
+			packet.setData(data);
+			socket.send(packet);
+	
+			}
+		}
+		catch (NumberFormatException e)
 		{
-			System.out.println("file non esistente");
-			doStream.writeUTF("-1"); 
+			e.printStackTrace();
+			System.exit(-1);
 		}
 		
-		else
+		catch(SocketException e )
 		{
-			doStream.writeUTF(table.get(nomeFile).toString());
-			System.out.println(table.get(nomeFile).toString());
+			System.err.println("SocketException ");
+			System.out.println("errore nella creazione della socket");
+			e.printStackTrace();
+			System.exit(-2);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(-3);
 		}
 		
-		data = boStream.toByteArray();
-		
-		
-		packet.setData(data);
-		socket.send(packet);
-
-		}
 		
 	}
 }
